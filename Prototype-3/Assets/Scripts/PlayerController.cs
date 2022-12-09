c84259941541c242bdb481c00dee17c11c305eaa
Juhwan Kim <juhwan.boxcat@gmail.com>
2022-12-09 12:18:00 +0900

    플레이어 점프 기능 추가
    플레이어가 space바를 통해 점프할 수 있도록 하였다. 또한 바닥에 있을 때에만 점프가 가능하도록 해서 이단점프를 금지하였다.

--- Prototype-3/Assets/Scripts/PlayerController.cs
+++ Prototype-3/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody _playerRb;
+    public float jumpForce = 15;
+    public float gravityModifier;
+    public bool isOnGround = true;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        _playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!Input.GetKeyDown(KeyCode.Space) || !isOnGround) return;
+        _playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        isOnGround = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
