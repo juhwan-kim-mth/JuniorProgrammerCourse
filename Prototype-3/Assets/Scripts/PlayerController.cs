344b8c287b6424ac7d8225ddc57ad77caa09dbf7
Juhwan Kim <juhwan.boxcat@gmail.com>
2022-12-15 13:33:21 +0900

    플레이어 애니메이션 추가
    점프 기능에 애니메이션을 추가하였고 장애물과 충돌 후 애니메이션을 추가하였다. 별도로 게임오버 시 더이상 점프하지 않도록 수정하였다.

--- Prototype-3/Assets/Scripts/PlayerController.cs
+++ Prototype-3/Assets/Scripts/PlayerController.cs
@@ -6,19 +6,24 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private bool onGround = true;
     private Rigidbody _playerRb;
+    private Animator _playerAnimator;
     [SerializeField] private float jumpForce;
     [SerializeField] private float gravityModifier;
     public bool gameOver;
+    private static readonly int JumpTrig = Animator.StringToHash("Jump_trig");
+    private static readonly int DeathB = Animator.StringToHash("Death_b");
+    private static readonly int DeathTypeINT = Animator.StringToHash("DeathType_int");
 
     private void Start()
     {
         _playerRb = GetComponent<Rigidbody>();
+        _playerAnimator = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     private void Update()
     {
-        if (onGround && Input.GetKeyDown(KeyCode.Space))
+        if (onGround && Input.GetKeyDown(KeyCode.Space) && !gameOver)
         {
             Jump();
         }
@@ -28,6 +33,7 @@ public class PlayerController : MonoBehaviour
     {
         _playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         onGround = false;
+        _playerAnimator.SetTrigger(JumpTrig);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -43,6 +49,8 @@ public class PlayerController : MonoBehaviour
             {
                 Debug.Log("Game Over");
                 gameOver = true;
+                _playerAnimator.SetBool(DeathB, true);
+                _playerAnimator.SetInteger(DeathTypeINT, 1);
                 break;
             }
         }
