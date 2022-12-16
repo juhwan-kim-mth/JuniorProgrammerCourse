b3933a9d302e0b5b8e5f1e1ed4e8aa9a928ba822
Juhwan Kim <juhwan.boxcat@gmail.com>
2022-12-16 17:50:34 +0900

    코드 작성 형식에 맞춤

--- Challenge-3/Assets/Scripts/SpinObjectsX.cs
+++ Challenge-3/Assets/Scripts/SpinObjectsX.cs
@@ -7,7 +7,7 @@ public class SpinObjectsX : MonoBehaviour
     public float spinSpeed;
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
     }
